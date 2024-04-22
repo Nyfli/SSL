b8e9a5ce514d1a6e6787ede702c0f5006abfee60
Nyfli <Nylpixe@gmail.com>
2024-04-22 12:52:28 +0200

    V4

--- Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
@@ -53,9 +53,14 @@ public class HeroEntity : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        _UpdateHorizontalSpeed();
-        _ChangeOrientFromHorizontalMovement();
+        if (_AreOrientAndMovementOpposite())
+        {
+            _TurnBack();
+        } else
+        {
+            _UpdateHorizontalSpeed();
+            _ChangeOrientFromHorizontalMovement();
+        }
         _ApplyHorizontalSpeed();
     }
 
@@ -83,6 +88,21 @@ public class HeroEntity : MonoBehaviour
         Vector3 newScale = _orientVisualRoot.localScale;
         newScale.x = _orientX;
         _orientVisualRoot.localScale = newScale;
+    }
+
+    private void _TurnBack()
+    {
+        _horizontalSpeed -= _movementsSettings.turnBackFriction * Time.fixedDeltaTime;
+        if (_horizontalSpeed < 0f)
+        {
+            _horizontalSpeed = 0f;
+            _ChangeOrientFromHorizontalMovement();
+        }
+    }
+
+    private bool _AreOrientAndMovementOpposite()
+    {
+        return _moveDirX * _orientX < 0f;
     }
 
     private void OnGUI()
--- Assets/SSL/Runtime/Scripts/Hero/HeroHorizontalMovementsSettings.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroHorizontalMovementsSettings.cs
@@ -10,5 +10,6 @@ public class HeroHorizontalMovementsSettings
 {
     public float acceleration = 20f;
     public float deceleration = 15f;
+    public float turnBackFriction = 25f;
     public float speedMax = 5f;
 }
