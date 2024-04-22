4797544d874ab98a2735c45d2a15eaf5c08fab56
Nyfli <Nylpixe@gmail.com>
2024-04-22 12:09:53 +0200

    v2

---
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    private void Update()
+    {
+        _entity.SetMoveDirX(GetInputMoveX());
+    }
+
+    [Header("Entity")]
+    [SerializeField] private HeroEntity _entity;
+
+    [Header("Debug")]
+    [SerializeField] private bool _guiDebug = false;
+
+    private void OnGUI()
+    {
+        if (!_guiDebug) return;
+
+        GUILayout.BeginVertical(GUI.skin.box);
+        GUILayout.Label(gameObject.name);
+        GUILayout.EndVertical();
+    }
+
+    private float GetInputMoveX()
+    {
+        float inputMoveX = 0f;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Q))
+        {
+            //Negative means : to the left
+            inputMoveX = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //Positive means : to the right
+            inputMoveX = 1f;
+        }
+
+        return inputMoveX;
+    }
+}
+
